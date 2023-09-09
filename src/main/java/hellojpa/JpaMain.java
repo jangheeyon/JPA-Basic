01d61d3d6d91cec3081d8d9840d2ca33508e50cd
jangheeyon <dev.hyjang@gmail.com>
2023-09-09 17:32:39 +0900

    :art: JPQL intro

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Iterator;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,7 +21,7 @@ public class JpaMain {
             member.setId(1L);
             member.setName("HelloA");
             em.persist(member);
-            */
+             */
             /* 멤버 조회
             Member findmember = em.find(Member.class, 1L);
             */
@@ -28,6 +30,19 @@ public class JpaMain {
             findmember.setName("helloJPA");
             //수정 후 저장은 하지 않아도 됨. jpa가 바뀐 것을 update해줌.
             */
+            
+            //JPQL로 쿼리를 생성할 수 있음
+            //단, JPA는 테이블을 대상으로 코드를 짜는게 아니라, Member 객체를 대상으로 함!
+            List<Member> result = em.createQuery("select m from Member as m", Member .class)
+                    //페이징 처리
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println(member.getName());
+            }
+
             tx.commit();    //정상작동 시 커밋
         } catch (Exception e) {
             tx.rollback();  //문제 생기면 롤백
