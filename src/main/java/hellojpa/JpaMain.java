638373b819da21b168e5a059cf5dfe2132693048
jangheeyon <dev.hyjang@gmail.com>
2023-09-15 00:49:37 +0900

    :ambulance: 객체지향적이지 않은 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,26 +8,33 @@ import javax.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //트랜젝션 중요(시작,종료를 적어 주어야 함)
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        //code를 작성
-        try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setUsername("A");
-            member.setRoleType(RoleType.USER);
 
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeamId(team.getId());
             em.persist(member);
 
-            tx.commit();    //정상작동 시 커밋 -> 이때 sql 보냄
+            Member findMember = em.find(Member.class, member.getId());
+            //멤버의 팀을 알고 싶으면? => 멤버 클래스에서 팀 아이디를 꺼내서, 팀에서 그 아이디를 꺼냄
+            Long findTeamId = findMember.getTeamId();
+            Team findTeam = em.find(Team.class, findTeamId);
+
+            tx.commit();
         } catch (Exception e) {
-            tx.rollback();  //문제 생기면 롤백
+            tx.rollback();
         } finally {
             em.close();
         }
-        //애플리케이션이 끝나면 닫아줌
         emf.close();
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,35 +1,18 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
-//@Table(name="Member")
 public class Member {
-    @Id
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name")
+
+    @Column(name = "USERNAME")
     private String username;
-    private Integer age;
-    //@Enumerated(EnumType.ORDINAL)을 쓰면 별도의 데이터 타입이 추가되면 순서에서 문제가 됨
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
 
-    //날짜 타입 최신 버전은 @Temporal 쓰지 않아도 됨
-    //private LocalDate testDate;
-    //private LocalDateTime testDateTime;
-    //날짜 타입 과거 버전
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob    //VARCHAR 보다 큰 컨텐츠
-    private String description;
-
-    public Member() {
-    }
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 
     public Long getId() {
         return id;
@@ -47,43 +30,11 @@ public class Member {
         this.username = username;
     }
 
-    public Integer getAge() {
-        return age;
+    public Long getTeamId() {
+        return teamId;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
     }
 }
